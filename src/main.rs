fd28731c8228531d64e6fc062b7a91533f1cdd4d
Aarav <ab1234now@gmail.com>
2025-05-22 00:11:04 -0400

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr:IpAddr,
+    threads: u16,
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 }
